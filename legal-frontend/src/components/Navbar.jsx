f3cf9b9295b2d58a54a9323dd9349cdc6496bcf9
Maliha <maliha16tabassum@gmail.com>
2025-11-06 16:13:14 +0600

    added scroll to top

--- legal-frontend/src/components/Navbar.jsx
+++ legal-frontend/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 export default function Navbar() {
+
+  const handleScroll = () =>{
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  }
   
   return (
     <div className="flex justify-center fixed top-0 z-10 w-full">
         <div className="navbar bg-base-100 shadow-sm ">
-    <div className="flex justify-center w-full">
-        <a className="btn btn-ghost text-lg !text-[#4CAF50]">Legally</a>
+    <div className="flex justify-evenly w-full">
+        <a onClick={handleScroll} className="btn btn-ghost !text-xl !font-bold !text-[#4CAF50]">Legally</a>
     </div>
     </div>
     </div>
